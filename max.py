6d8b459bb45887fb5444fb90e483a45ea6025080
JohnMartin-19 <67791294+JohnMartin-19@users.noreply.github.com>
2023-05-15 10:02:22 +0300

    correction

--- max.py
+++ max.py
@@ -1,3 +1,10 @@
-x=[2,4,5,8,6,1]
-print(len(x))
-print(max(x))
+largest = -1
+print(largest)
+for i in [2,4,5,8,6,1] :
+    if i > largest :
+        largest = i
+    print(largest,i)
+
+print('Largest:',largest)
+
+
